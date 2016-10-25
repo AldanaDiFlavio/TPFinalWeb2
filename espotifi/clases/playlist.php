a9ab78ebff4d0b3cfbb9c0035877a32b69227251
Matias Martinez <martinezms@outlook.com>
2016-10-25 12:30:39 -0300

    Nuevo!
    Vamos trabajar en esta carpeta "espotifi".
    Asi tratamos de ir poniendo en limpio todo.
    En "clases" van las clases que vayan armando, en este caso playlist la
    mia.
    En html van las "vistas", no necesariamente .html.
    En php va todo lo que es intermediario entre html y clases, cosas como
    validaciones y manipulacion de datos.
    En phpmyadmin exporte la base de datos, cada vez que hagan cambios en la
    base de datos, cambien el DER del DRIVE, exportenla desde phpmyadmin y
    subanla a git.

---
+++ espotifi/clases/playlist.php
@@ -0,0 +1,125 @@
+<?php
+include('db.php');
+
+class playlist {
+			
+			private $idPlaylist;
+			
+			public function __construct($idPlaylist)
+										{
+										$this->idPlaylist = $idPlaylist;										
+										}
+			
+			public static function crearPlaylist($nombre){
+				$db = new BaseDatos();
+				$date = date('y-m-d');
+				if($db->conectar()){
+						$consultar = "INSERT INTO playlist (nombre, fecha_creacion) VALUES ('$nombre', '$date')";
+						mysqli_query( $db->conexion, $consultar) or die("No se pudo conectar.");
+				}
+				$db->desconectar();
+			}
+			
+			public static function buscarPlaylist(){ //Esta incompleto, falta pasar como parametro el ID del creador, y asi filtrar solo las que le pertenecen a el.
+				$db = new BaseDatos();			//Luego en $buscarPlaylist se adisiona al WHERE codDueño = idUsuario.
+				if($db->conectar()){
+						$buscarPlaylist = "SELECT p.idPlaylist id, p.nombre Nombre, e.descripcion Estado,  p.fecha_creacion Creada FROM playlist p JOIN estado e ON p.codEstado = e.idEstado WHERE baneo = 0;";
+						$resultado = mysqli_query( $db->conexion, $buscarPlaylist) or die("No se pudo conectar.");
+						echo "<select name='playlist'>";   
+						while ($row = mysqli_fetch_assoc($resultado)){    
+							echo "<option value='$row[id]'>$row[Nombre] $row[Estado]</option>";    
+						}  
+						echo "</select> ";  
+			
+				}
+				$db->desconectar();
+			}
+			
+			public static function buscarCanciones(){ // Este metodo es auxiliar, ya que no pertenece a la clase Playlist.
+				$db = new BaseDatos();			
+				if($db->conectar()){
+						$buscaCanciones = "SELECT idCancion id, titulo, album, artista, duracion FROM cancion WHERE baneo = 0;";
+						$resultado = mysqli_query( $db->conexion, $buscaCanciones) or die("No se pudo conectar.");
+						
+						while ($row = mysqli_fetch_assoc($resultado)){    
+							echo "<input type='checkbox' name='check_list[]' value='$row[id]'>$row[titulo]</input><br>";    
+						}  
+						 
+			
+				}
+				$db->desconectar();
+			}
+			
+			
+			public function agregarCancion($codCancion){
+				$db = new BaseDatos();
+				if($db->conectar()){
+						$consultar = "INSERT INTO contiene (codPlaylist, codCancion) VALUES ('$this->idPlaylist', '$codCancion')";
+						mysqli_query( $db->conexion, $consultar) or die("Algunas canciones seleccionadas ya estan en el setlist.");
+				}
+				$db->desconectar();
+			}
+			
+			public function asignarEstado($codEstado){
+				$db = new BaseDatos();
+				if($db->conectar()){
+						$consultar = "UPDATE playlist SET codEstado = '$codEstado' WHERE idPlaylist = '$this->idPlaylist'";
+						mysqli_query( $db->conexion, $consultar) or die("No se pudo conectar.");
+				}
+				$db->desconectar();
+			}
+			
+			public function asignarEsquema($codEsquema){
+				$db = new BaseDatos();
+				if($db->conectar()){
+						$consultar = "UPDATE playlist SET codEsquema = '$codEsquema' WHERE idPlaylist = '$this->idPlaylist'";
+						mysqli_query( $db->conexion, $consultar) or die("No se pudo conectar.");
+				}
+				$db->desconectar();
+			}
+			
+			public function asignarGenero($codGenero){ 
+				$db = new BaseDatos();
+				if($db->conectar()){
+						$consultar = "INSERT INTO pertenece (codElemento, codGenero) VALUES ('$this->idPlaylist', '$codGenero')";
+						mysqli_query( $db->conexion, $consultar) or die("No se pudo conectar.");
+				}
+				$db->desconectar();
+			}
+	
+			public static function listarGenerosPlaylist(){
+				$db = new BaseDatos();
+				if($db->conectar()){
+						$buscaGeneros = "SELECT idGenero id, descripcion FROM genero;";
+						$resultado = mysqli_query( $db->conexion, $buscaGeneros) or die("No se pudo conectar.");
+						echo "<select name='generos'>";   
+						while ($row = mysqli_fetch_assoc($resultado)){    
+							echo "<option value='$row[id]'>$row[descripcion]</option>";    
+						}  
+						echo "</select> ";  
+				}
+				$db->desconectar();
+			}
+			
+			public static function topCinco(){
+				$db = new BaseDatos();
+				if($db->conectar()){
+					$buscarCincoMejores = "SELECT nombre, cantidad_votos FROM playlist ORDER BY cantidad_votos DESC LIMIT 0,5;";
+					$resultado = mysqli_query( $db->conexion, $buscarCincoMejores) or die("No se pudo conectar.");
+
+						while ($row = mysqli_fetch_assoc($resultado)){    
+							echo "'$row[nombre]'<br>";    
+						}  
+						  
+					
+					
+					
+				}
+				$db->desconectar();
+			}
+			
+}
+
+
+
+?>
---
+++ espotifi/php/crearPlaylist.php
@@ -0,0 +1,62 @@
+<?php
+	include('playlist.php');
+	
+	$accion = $_POST['accion'];
+	
+	if ($accion == "crear"){
+		$nombre = $_POST['nombre'];
+		
+		playlist::crearPlaylist($nombre);  		
+	}	
+	
+	if ($accion == "agregar"){
+		$idPlaylist = $_POST['playlist'];
+		
+		$db = new BaseDatos();
+				if($db->conectar()){															
+							foreach($_POST['check_list'] as $check) {
+								$idCancion = "$check";								
+								$playlist = new playlist($idPlaylist);
+								$playlist->agregarCancion($idCancion);
+							}    			 
+				}
+				$db->desconectar();
+	}
+	
+	if ($accion == "cambiar estado"){
+		$idPlaylist = $_POST['playlist'];
+		$idEstado = $_POST['estado'];
+		
+		$db = new BaseDatos();
+				if($db->conectar()){						
+							$playlist = new playlist($idPlaylist);
+							$playlist->asignarEstado($idEstado);   				 
+				}
+				$db->desconectar();
+	}
+	
+	if ($accion == "cambiar esquema"){
+		$idPlaylist = $_POST['playlist'];
+		$idEsquema = $_POST['esquema'];
+		
+		$db = new BaseDatos();
+				if($db->conectar()){						
+							$playlist = new playlist($idPlaylist);
+							$playlist->asignarEsquema($idEsquema);   
+				}
+				$db->desconectar();
+	}
+	
+	if ($accion == "cambiar genero de playlist"){
+		$idPlaylist = $_POST['playlist'];
+		$idGenero = $_POST['generos'];
+		
+		$db = new BaseDatos();
+				if($db->conectar()){							
+							$playlist = new playlist($idPlaylist);
+							$playlist->asignarGenero($idGenero);   		  
+				}
+				$db->desconectar();
+	}
+?>
+
