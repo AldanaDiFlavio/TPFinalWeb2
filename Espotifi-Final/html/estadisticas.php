d9a22d34044534df4f92367a64cd8e65756a23f0
AldanaDiFlavio <aldanadiflavio@live.com.ar>
2016-11-22 11:33:50 -0300

    graficos
    falta el el grafico de las fechas esta incompleto ese solo no logro hacer q lo muestre en el grafico pero el sql funciona, lo dejo en forma de torta porque en columan se ve mal

---
+++ Espotifi-Final/html/estadisticas.php
@@ -0,0 +1,143 @@
+<?php
+session_start();
+	
+	if ($_SESSION['login'] == "on")
+		{
+		include_once('../clases/administrador.php');
+		$_SESSION['admin'] = 'true';
+		}else{
+			 header('location: ../html/index.php'); 
+			 }  
+	
+									
+?>
+<html>
+  	<head>
+	    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+	    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+	    <script src="http://code.jquery.com/jquery-latest.js"></script>
+	    <script type="text/javascript">
+    
+	    $(function(){
+	    	google.charts.load('current', {'packages':['corechart','table']});
+
+	      	google.charts.setOnLoadCallback(loadData);
+	    });
+      
+      	function loadData() {
+      		//periodo
+      		$.ajax({
+	      		url:"../php/cantPlaylistPeriodo.php",
+	      		dataType:"JSON",
+	      		success: drawChartPlaylistPeriodo
+	      	});
+      		//reproducciones
+	      	$.ajax({
+	      		url:"../php/rankingCantReproducciones.php",
+	      		dataType:"JSON",
+	      		success: drawChartCantReproducciones
+	      	});
+	      	//votos
+	      	$.ajax({
+	      		url:"../php/rankingCantPlaylistVotos.php",
+	      		dataType:"JSON",
+	      		success: drawChartCantVotos
+	      	});
+	      	//bann
+	        $.ajax({
+	      		url:"../php/cantUsuariosBanneados.php",
+	      		dataType:"JSON",
+	      		success: drawChartCantUsuariosBann
+	      	});
+	        //pais
+	      	$.ajax({
+	      		url:"../php/cantUsuariosPais.php",
+	      		dataType:"JSON",
+	      		success: drawChartCantUsuariosPais
+	      	});
+      	}
+      	//periodo
+      	function drawChartPlaylistPeriodo(jsonData){
+	      	var pieData = new google.visualization.arrayToDataTable(jsonData);
+	      	var chart = new google.visualization.PieChart(document.getElementById('cantPlaylistPeriodoChart'));
+	      	chart.draw(pieData,{title: '', width:500, height:300});
+	      	var table = new google.visualization.Table(document.getElementById('cantPlaylistPeriodoTable'));
+	      	table.draw(pieData,{showRowNumber: true, width: '60%'});
+      	}
+
+      //reproducciones
+      	function drawChartCantReproducciones(jsonData){
+	      	var pieData = new google.visualization.arrayToDataTable(jsonData);
+	      	var chart = new google.visualization.PieChart(document.getElementById('rankingCantReproduccionesChart'));
+	      	chart.draw(pieData,{title: '', width:500, height:300});
+	      	var table = new google.visualization.Table(document.getElementById('rankingCantReproduccionesTable'));
+	      	table.draw(pieData,{showRowNumber: true, width: '60%'});
+      	}
+
+      //votos
+      	function drawChartCantVotos(jsonData){
+	      	var pieData = new google.visualization.arrayToDataTable(jsonData);
+	      	var chart = new google.visualization.PieChart(document.getElementById('rankingCantPlaylistVotosChart'));
+	      	chart.draw(pieData,{title: '', width:500, height:300});
+	      	var table = new google.visualization.Table(document.getElementById('rankingCantPlaylistVotosTable'));
+	      	table.draw(pieData,{showRowNumber: true, width: '60%'});
+      	}
+		//bann
+      	function drawChartCantUsuariosBann(jsonData){
+	      	var pieData = new google.visualization.arrayToDataTable(jsonData);
+	      	var chart = new google.visualization.PieChart(document.getElementById('cantUsuariosBanneadosChart'));
+	      	chart.draw(pieData,{title: '', width:500, height:300});
+	      	var table = new google.visualization.Table(document.getElementById('cantUsuariosBanneadosTable'));
+	      	table.draw(pieData,{showRowNumber: true, width: '60%'});
+      	}
+      	//pais
+      	function drawChartCantUsuariosPais(jsonData){
+	      	var pieData = new google.visualization.arrayToDataTable(jsonData);
+	      	var chart = new google.visualization.PieChart(document.getElementById('cantUsuariosPaisChart'));
+	      	chart.draw(pieData,{title: '', width:500, height:300});
+	      	var table = new google.visualization.Table(document.getElementById('cantUsuariosPaisTable'));
+	      	table.draw(pieData,{showRowNumber: true, width: '60%'});
+      	}
+      
+      
+    </script>
+  </head>
+  	<body class=>
+  		<h1>Estadisticas</h1>
+  		
+		<div>
+			<h3>Cantidad de playlist creada en un periodo determinado</h3>
+			<form id="form" name="form" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+				Fecha Inicio:
+				<input type="date" name="fechaIni" class="datepicker">
+				<br>
+				Fecha Final:
+				<input type="date" name="fechaFin" class="datepicker">
+				<br>
+				<input type="submit" value="Consultar">
+			</form>
+  			<div id="cantPlaylistPeriodoChart" style="border: 1px solid #ccc"></div>
+       		<div id="cantPlaylistPeriodoTable" style="border: 1px solid #ccc"></div>
+		</div>
+  		<div>
+  			<h3>Ranking de playlist por cantidad de reproducciones</h3>
+  			<div id="rankingCantReproduccionesChart" style="border: 1px solid #ccc"></div>
+      		<div id="rankingCantReproduccionesTable" style="border: 1px solid #ccc"></div>
+  		</div>
+  		<div>
+  			<h3>Ranking de playlist por votos</h3>
+  			<div id="rankingCantPlaylistVotosChart" style="border: 1px solid #ccc"></div>
+      	   	<div id="rankingCantPlaylistVotosTable" style="border: 1px solid #ccc"></div>
+  		</div>
+  		<div>
+  			<h3>Cantidad de usuarios banneados</h3>
+  			<div id="cantUsuariosBanneadosChart" style="border: 1px solid #ccc"></div>
+      	   	<div id="cantUsuariosBanneadosTable" style="border: 1px solid #ccc"></div>
+  		</div>
+  		<div>
+  			<h3>Cantidad de usuarios por pais</h3>
+  			<div id="cantUsuariosPaisChart" style="border: 1px solid #ccc"></div>
+      		<div id="cantUsuariosPaisTable" style="border: 1px solid #ccc"></div>
+  		</div>
+  	</body>
+</html>
