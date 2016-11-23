4399c995c4ea24125329c96b1330a3e866b97bd2
AldanaDiFlavio <aldanadiflavio@live.com.ar>
2016-11-22 23:40:08 -0300

    archivos actualizados

--- Espotifi-Final/php/cantPlaylistPeriodo.php
+++ Espotifi-Final/php/cantPlaylistPeriodo.php
@@ -3,24 +3,43 @@
 
       	$db = new BaseDatos();
       	
-      	$fechaIni=$_POST['fechaIni'];
-		$fechaFin=$_POST['fechaFin'];
-      	
+
+
+      	$fechaIni=$_GET['fechaIni'];
+		$fechaFin=$_GET['fechaFin'];
+		$FInicial = date('Y-m-d', strtotime($fechaIni));
+		$FFinal =  date('Y-m-d', strtotime($fechaFin));
+		
+
+
       	if($db->conectar()){
-			$sql = "SELECT nombre AS Playlist, fecha_creacion AS Fecha FROM playlist WHERE fecha_creacion BETWEEN '$fechaIni' AND '$fechaFin' ORDER BY fecha_creacion DESC;";
-			$result = mysqli_query( $db->conexion, $sql) or die("Error al listar usuarios.");
-			$dataTable = array(array("Playlist", "Cantidad"));
 
+      		if(empty($fechaIni) || empty($fechaFin)){
+      			$sql = "SELECT nombre AS Playlist, fecha_creacion AS Fecha FROM playlist;";
+      			$result = mysqli_query( $db->conexion, $sql) or die("Error al listar usuarios.");
+				$dataTable = array(array("Playlist", "Fecha"));
 
-			while($fila = mysqli_fetch_assoc($result)){
-				 $dataTable[] = array($fila['Playlist'], (int)$fila['Cantidad']);
-
+				while($fila = mysqli_fetch_assoc($result)){
+				 	$dataTable[] = array($fila['Playlist'], $fila['Fecha']);
 				}
-			$db->desconectar();
-			echo json_encode($dataTable);
-			location '../html/estadisticas.php';
+				$db->desconectar();
+				echo json_encode($dataTable);
+      		}
+      		else{
+      			$sql = "SELECT nombre AS Playlist, fecha_creacion AS Fecha FROM playlist WHERE fecha_creacion BETWEEN '$FInicial' AND '$FFinal';";
+
+				$result = mysqli_query( $db->conexion, $sql) or die("Error al listar usuarios.");
+				$dataTable = array(array("Playlist", "Fecha"));
+
+
+				while($fila = mysqli_fetch_assoc($result)){
+				 	$dataTable[] = array($fila['Playlist'], $fila['Fecha']);		
+				}
+				$db->desconectar();
+				echo json_encode($dataTable);
+
+      		}
 		}
 		
 
-		
 ?>
--- Espotifi-Final/php/cantUsuariosPais.php
+++ Espotifi-Final/php/cantUsuariosPais.php
@@ -3,17 +3,17 @@
       	$db = new BaseDatos();
       	
       	if($db->conectar()){
-			$sql = "SELECT count(*) as Cantidad, ubicacion FROM usuario GROUP BY ubicacion;";
+			$sql = "SELECT count(*) as Cantidad, pais FROM usuario GROUP BY pais;";
 			$result = mysqli_query( $db->conexion, $sql) or die("Error al listar usuarios.");
 			$dataTable = array(array("País", "Cantidad"));
 
 			while($fila = mysqli_fetch_assoc($result)){
 
-				if(($fila['ubicacion'] == "") || (is_null($fila['ubicacion']))){
+				if(($fila['pais'] == "") || (is_null($fila['pais']))){
 					$dataTable[] = array('Otros', (int)$fila['Cantidad']);
 				} 
 				else{ 
-					$dataTable[] = array($fila['ubicacion'], (int)$fila['Cantidad']);
+					$dataTable[] = array($fila['pais'], (int)$fila['Cantidad']);
 				}
 		}
 			$db->desconectar();
